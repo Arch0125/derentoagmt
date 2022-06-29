bdcf16c1690f587e95b000c7e9c7a2ec350d03ad
Arch0125 <dasarchisman25@gmail.com>
2022-06-30 02:05:42 +0530

    Project Complete

--- src/components/CreateAgmt.js
+++ src/components/CreateAgmt.js
@@ -3,6 +3,8 @@ import React from 'react';
 import { Input,InputLeftAddon,InputGroup,Stack } from '@chakra-ui/react';
 import { useState } from 'react';
 import GetContract from '../hooks/GetContract';
+import emailjs from '@emailjs/browser';
+import { useRef } from 'react';
 
 const CreateAgmt = () => {
 
@@ -13,16 +15,32 @@ const CreateAgmt = () => {
     const[duration,setDuration]=useState('');
     const[productid,setProductid]=useState('');
     const[ipfshash,setIpfshash]=useState('');
+    const[tosend,setTosend]=useState([]);
 
     const contract = GetContract();
+    const form = useRef();
 
     const createagmt=async()=>{
         await contract.addAgreement(tenant,lmail,tmail,amount,duration,productid,ipfshash)
     }
 
+    const sendEmail = (e) => {
+        e.preventDefault();
+        console.log(form.current)
+        emailjs.sendForm('service_zexdado', 'template_peblt5r', form.current, 'uCXyLxudrrEnzzx90')
+          .then((result) => {
+              console.log(result.text);
+          }, (error) => {
+              console.log(error.text);
+          });
+      };
+
+
+
     return ( 
         <Flex flexDirection={"column"} width={"80%"} p={'20px'} mt={'8%'} height={'fit-content'} bgColor={'white'} borderWidth={'2px'} rounded={'2xl'} borderColor={'blackAlpha.200'} boxShadow={'xl'}  >
             <Text fontWeight={"semibold"} fontSize={'2xl'} textAlign={"center"} color={"purple.800"} >Create Agreement</Text>
+            <form ref={form} onSubmit={sendEmail} > 
             <Stack spacing={4} >
                 <InputGroup mt={"20px"}>
                     <InputLeftAddon bgColor={'gray.500'} >Tenant Address</InputLeftAddon>
@@ -34,7 +52,7 @@ const CreateAgmt = () => {
                 </InputGroup>
                 <InputGroup mt={"20px"}>
                     <InputLeftAddon bgColor={'gray.500'} >Tenant Mail</InputLeftAddon>
-                    <Input onChange={e=>setTmail(e.target.value)} color={"blackAlpha.700"} borderColor={'gray.400'} />
+                    <Input onChange={e=>setTmail(e.target.value)} color={"blackAlpha.700"} name='user_email' borderColor={'gray.400'} />
                 </InputGroup>
                 <InputGroup mt={"20px"}>
                     <InputLeftAddon bgColor={'gray.500'} >Amount</InputLeftAddon>
@@ -53,7 +71,8 @@ const CreateAgmt = () => {
                     <Input onChange={e=>setIpfshash(e.target.value)} color={"blackAlpha.700"} borderColor={'gray.400'} />
                 </InputGroup>
             </Stack>
-            <Button onClick={createagmt} variant={"solid"} colorScheme={'purple'} color={'white'} mt={"40px"} >Publish on Blockchain 🚀</Button>
+            <Button onClick={createagmt} width={"100%"} type='submit' variant={"solid"} colorScheme={'purple'} color={'white'} mt={"40px"} >Publish on Blockchain 🚀</Button>
+            </form>
         </Flex>
      );
 }
--- src/components/SignAgmt.js
+++ src/components/SignAgmt.js
@@ -2,6 +2,8 @@ import React, { useState } from 'react';
 import { Button, Divider, Flex,Text,Box,Link } from '@chakra-ui/react';
 import GetContract from '../hooks/GetContract';
 import GetAccount from '../hooks/GetAccount';
+import emailjs from '@emailjs/browser'
+import { useRef } from 'react';
 
 const SignAgmt = () => {
 
@@ -10,6 +12,8 @@ const SignAgmt = () => {
     const[agmts,setAgmts]=useState([]);
 
     const account = GetAccount();
+
+    const form = useRef();
 
     const showAgmt = async()=>{
         var listcount = await contract.getCount();
@@ -30,6 +34,17 @@ const SignAgmt = () => {
         await contract.signAgreement(index);
      }
 
+     const sendEmail = (e) => {
+        e.preventDefault();
+        console.log(form.current)
+        emailjs.sendForm('service_zexdado', 'template_8q94tnl', form.current, 'uCXyLxudrrEnzzx90')
+          .then((result) => {
+              console.log(result.text);
+          }, (error) => {
+              console.log(error.text);
+          });
+      };
+
     return ( 
         <Flex  flexDirection={"column"} width={"95%"} p={'20px'} mt={'8%'} height={'fit-content'} bgColor={'white'} borderWidth={'2px'} rounded={'2xl'} borderColor={'blackAlpha.200'} boxShadow={'xl'}  >
             <Text fontSize={"2xl"} fontWeight={"semibold"} color={'purple.700'} textAlign={'center'} mb={"20px"} >Agreements Received</Text>
@@ -38,6 +53,8 @@ const SignAgmt = () => {
             {
                 Object.keys(agmts).map((agmt, index) => (
                     <Flex color={"blackAlpha.800"} flex={"1"} flexDirection={"row"} width={"100%"} height={"fit-content"} padding={"20px"} bgColor={"gray.50"} mt={"15px"} rounded={"2xl"} borderWidth={"1.5px"} borderColor={"gray.100"} >
+                        
+                        
                         <Text>Agreement ID #{(agmts[index].agmtid).toString()} &nbsp; </Text>
                         <Box width={"50%"}>
                             <Text>Lender : {(agmts[index].lender).substring(0,7)}</Text>
@@ -46,7 +63,14 @@ const SignAgmt = () => {
                         </Box>
                         <Box  >
                         <Text>Duration : {(agmts[index].duration)}</Text>
-                        <Button onClick={() => signAgmt((agmts[index].agmtid).toString())} variant={"solid"} colorScheme={"purple"} mt={'5px'} >Sign Agreement</Button>
+                        <form ref={form} onSubmit={sendEmail}>
+                            <input type={'hidden'} name='lender' value={agmts[index].lender} />
+                            <input type={'hidden'} name='tenant' value={agmts[index].tenant} />
+                            <input type={'hidden'} name='agmtid' value={(agmts[index].agmtid)} />
+                            <input type={'hidden'} name='lmail' value={agmts[index].lmail} />
+                            <input type={'hidden'} name='tmail' value={agmts[index].tmail} />
+                        <Button onClick={() => signAgmt((agmts[index].agmtid).toString())} type={'submit'} variant={"solid"} colorScheme={"purple"} mt={'5px'} >Sign Agreement</Button>
+                        </form>
                         </Box>
                     </Flex>
                 ))
